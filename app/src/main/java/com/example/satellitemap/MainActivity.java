443bf7f89fdda1d6cd074e69b1d5b7be98b08fe5
SDragonSEN <SDragonSEN@163.com>
2017-10-04 23:06:04 +0800

    初步完成星图的绘制,剩余工作：1、完善剩余界面;2、增加随指南针而旋转的动态效果；3、增加扫描过程的动态效果；4、增加左右切换的功能

--- app/src/main/java/com/example/satellitemap/MainActivity.java
+++ app/src/main/java/com/example/satellitemap/MainActivity.java
@@ -3,25 +3,25 @@ package com.example.satellitemap;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.location.GnssStatus;
-import android.location.GpsSatellite;
-import android.location.GpsStatus;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.util.DisplayMetrics;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import static android.location.GnssStatus.*;
 
@@ -29,15 +29,43 @@ public class MainActivity extends AppCompatActivity {
     private LocationManager mLocationManager = null;
     private TextView mTextView;
     private Handler mHandler;
+    private MyView myView;
+    private ArrayList<MyStatellite> myStatellites = new ArrayList<MyStatellite>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        DisplayMetrics dm =getResources().getDisplayMetrics();
+
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.linearLyout);
+
+        myView = new MyView(this);
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(dm.widthPixels, dm.widthPixels);
+        myView.setBackgroundColor(Color.parseColor(MyView.BACKGROUND_COLOR1));
+        myView.setLayoutParams(lp);
+
+        linearLayout.addView(myView, 0);
 
         mTextView = (TextView)findViewById(R.id.test);
         mHandler = new Handler(getMainLooper());
 
+      /*  mTextView.post(new Runnable() {
+            @Override
+            public void run() {
+
+                LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) view.getLayoutParams();
+                layoutParams.height = layoutParams.width;
+                Log.e("ZSZS",layoutParams.width+"");
+                view.setLayoutParams(layoutParams);
+            }
+        });*/
         mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
@@ -97,6 +125,9 @@ public class MainActivity extends AppCompatActivity {
                 return;
 
             final StringBuffer sb = new StringBuffer();
+
+            myStatellites.clear();
+
             sb.append("卫星状态：\n");
             for (int i = 0; i < numb; i++){
                 sb.append(Integer.toString(i));
@@ -141,6 +172,14 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
                 sb.append("\n");
+                MyPosition myPosition = new MyPosition(status.getConstellationType(i), status.getElevationDegrees(i), status.getAzimuthDegrees(i));
+                sb.append("radium:\t");
+                sb.append(myPosition.radium);
+                sb.append("\n");
+
+                myStatellites.add(new MyStatellite(status.getConstellationType(i),status.getSvid(i),status.getAzimuthDegrees(i),myPosition.radium));
+
+                myView.invalidate();
             }
             sb.append("\n");
             Runnable r = new Runnable() {
@@ -152,4 +191,148 @@ public class MainActivity extends AppCompatActivity {
             mHandler.post(r);
         }
     };
+    class MyView extends View{
+        public static final String BACKGROUND_COLOR1 = "#004400";
+        public static final String BACKGROUND_COLOR2 = "#00aa00";
+
+        public static final String LINE_COLOR1 = "#002200";
+        public static final String LINE_COLOR2 = "#208020";
+
+        public static final String SATELLITE_COLOR_BEIDU = "#cc0000";
+        public static final String SATELLITE_COLOR_GPS = "#0000cc";
+        public static final String SATELLITE_COLOR_GLONASS = "#00cccc";
+        public static final String SATELLITE_COLOR_GALILEO = "#f19cc2";
+        public static final String SATELLITE_COLOR_QZSS = "#ffffff";
+        public static final String SATELLITE_COLOR_OTHER = "#999999";
+        public static final int MAX_DISTANCE = 30000;
+
+        private int mapRadium;
+        private int mapCenterX;
+        private int mapCenterY;
+        public MyView(Context context){
+            super(context);
+
+            DisplayMetrics dm =getResources().getDisplayMetrics();
+            mapRadium = dm.widthPixels / 2 - 50;
+            mapCenterX = dm.widthPixels / 2;
+            mapCenterY = dm.widthPixels / 2;
+        }
+        @Override
+        public void onDraw(Canvas canvas){
+            Paint paint = new Paint();
+
+            // 显示仪背景
+            paint.setColor(Color.parseColor(BACKGROUND_COLOR2));
+            paint.setStrokeWidth(1);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawCircle(mapCenterX, mapCenterY, mapRadium, paint);
+
+            //内圆
+            paint.setColor(Color.parseColor(LINE_COLOR2));
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(8);
+            canvas.drawCircle(mapCenterX, mapCenterY, (int)(mapRadium * 0.2), paint);
+            canvas.drawCircle(mapCenterX, mapCenterY, (int)(mapRadium * 0.4), paint);
+            canvas.drawCircle(mapCenterX, mapCenterY, (int)(mapRadium * 0.6), paint);
+            canvas.drawCircle(mapCenterX, mapCenterY, (int)(mapRadium * 0.8), paint);
+
+            //斜线
+            canvas.drawLine(mapCenterX + mapRadium, mapCenterY, mapCenterX - mapRadium, mapCenterY, paint);
+            canvas.drawLine(mapCenterX, mapCenterY + mapRadium, mapCenterX, mapCenterY - mapRadium, paint);
+            float d = (float)(mapRadium * Math.sin(Math.PI / 4));
+            canvas.drawLine(mapCenterX - d , mapCenterY - d, mapCenterX + d, mapCenterY + d, paint);
+            canvas.drawLine(mapCenterX - d , mapCenterY + d, mapCenterX + d, mapCenterY - d, paint);
+
+            //画中心点
+            paint.setStrokeWidth(1);
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.parseColor(LINE_COLOR1));
+            canvas.drawCircle(mapCenterX, mapCenterY, 10, paint);
+
+            //外圆;
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(10);
+            canvas.drawCircle(mapCenterX, mapCenterY, mapRadium, paint);
+
+            for (MyStatellite myStatellite : myStatellites){
+                paint.setStrokeWidth(1);
+                paint.setStyle(Paint.Style.FILL);
+                switch (myStatellite.type) {
+                    case CONSTELLATION_BEIDOU:
+                        paint.setColor(Color.parseColor(SATELLITE_COLOR_BEIDU));
+                        break;
+                    case CONSTELLATION_GALILEO:
+                        paint.setColor(Color.parseColor(SATELLITE_COLOR_GALILEO));
+                        break;
+                    case CONSTELLATION_GLONASS:
+                        paint.setColor(Color.parseColor(SATELLITE_COLOR_GLONASS));
+                        break;
+                    case CONSTELLATION_GPS:
+                        paint.setColor(Color.parseColor(SATELLITE_COLOR_GPS));
+                        break;
+                    case CONSTELLATION_QZSS:
+                        paint.setColor(Color.parseColor(SATELLITE_COLOR_QZSS));
+                        break;
+                    case CONSTELLATION_SBAS:
+                        paint.setColor(Color.parseColor(SATELLITE_COLOR_OTHER));
+                        break;
+                    case CONSTELLATION_UNKNOWN:
+                    default:
+                        paint.setColor(Color.parseColor(SATELLITE_COLOR_OTHER));
+                        break;
+                }
+                float x = (float)(mapCenterX - Math.cos(myStatellite.azimuthDegrees * Math.PI / 180) * myStatellite.radium / MAX_DISTANCE * this.mapRadium);
+                float y = (float)(mapCenterY + Math.sin(myStatellite.azimuthDegrees * Math.PI / 180) * myStatellite.radium / MAX_DISTANCE * this.mapRadium);
+                canvas.drawCircle(x, y, 10, paint);
+            }
+
+            super.onDraw(canvas);
+        }
+    }
+}
+class MyPosition{
+    double  radium;
+    float  azimuthDegrees;//方位角,角度制
+    final int earthRadium = 6371;
+    MyPosition(int gnssType, float elevationDegrees, float azimuthDegrees){
+        this.azimuthDegrees = azimuthDegrees;
+
+        int r1 = earthRadium;
+        int r2 = earthRadium + getHeight(gnssType);
+        double alpha = (elevationDegrees/180)*Math.PI;//转成弧度制
+
+        double cosA_2_r1 = 2 * r1 * Math.cos(alpha + Math.PI / 2);
+        double theta = Math.pow(cosA_2_r1,2) - 4 * (Math.pow(r1,2 ) - Math.pow(r2, 2));
+        this.radium = (( cosA_2_r1 + Math.sqrt(theta) ) / 2) * Math.cos(alpha);
+    }
+    static int getHeight(int gnssType){
+        switch (gnssType){
+            case CONSTELLATION_BEIDOU:
+                return 21500;
+            case CONSTELLATION_GALILEO:
+                return 23222;
+            case CONSTELLATION_GLONASS:
+                return 19100;
+            case CONSTELLATION_GPS:
+                return 20200;
+            case CONSTELLATION_QZSS:
+                //return 36786;
+            case CONSTELLATION_SBAS:
+            case CONSTELLATION_UNKNOWN:
+            default:
+                return 20000;
+        }
+    }
+}
+class MyStatellite{
+    public int type;
+    public int id;
+    public float azimuthDegrees;//方位角,以北为正方向,角度制
+    public double radium;
+    public MyStatellite(int type, int id, float azimuthDegrees, double radium){
+        this.type = type;
+        this.id = id;
+        this.azimuthDegrees = azimuthDegrees;
+        this.radium = radium;
+    }
 }
